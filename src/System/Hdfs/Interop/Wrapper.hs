465ff5dc0bac2ea3c5c7a87792d6ecca6ddde8ab
Andrei Varanovich <dotnetby@gmail.com>
2012-04-06 17:23:50 +0200

    initial export

---
+++ src/System/Hdfs/Interop/Wrapper.hs
@@ -0,0 +1,11 @@
+
+{-# LANGUAGE CPP, ForeignFunctionInterface #-}
+
+module System.Hdfs.Interop.Wrapper where
+
+import Foreign
+import Foreign.C.Types
+import Foreign.C.String
+
+--TODO: import all foreign functions
+#include <hdfs.h>
